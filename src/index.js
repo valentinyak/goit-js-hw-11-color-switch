5b96919f14ffd1cc618313685adcaf0cfc8420f8
valentinyak <63512456+valentinyak@users.noreply.github.com>
2020-10-20 21:00:28 +0300

    Добавляет код выполнения ДЗ/1

--- src/index.js
+++ src/index.js
@@ -6,3 +6,29 @@ const colors = [
   '#009688',
   '#795548',
 ];
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+};
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function onStartBtnClick() {
+  intervalId = setInterval(() => {
+    let randomIndex = randomIntegerFromInterval(0, colors.length - 1);
+
+    document.body.style.backgroundColor = colors[randomIndex];
+  }, 1000);
+  refs.startBtn.disabled = true;
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+  refs.startBtn.disabled = false;
+}
